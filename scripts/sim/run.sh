2c0c4b1c1303db638d8bc1aea9fc808cea03156e
langmead <langmead@208de1ca-060f-4dda-baf0-1f374fee3c76>
2011-03-24 20:46:54 +0000

    Die if make fails instead of running the test on old binaries

--- scripts/sim/run.sh
+++ scripts/sim/run.sh
@@ -2,7 +2,7 @@
 
 #  run.sh
 
-make bowtie2 bowtie2-debug bowtie2-build bowtie2-build-debug
+make bowtie2 bowtie2-debug bowtie2-build bowtie2-build-debug && \
 perl scripts/sim/run.pl \
 	--bowtie2=./bowtie2 \
 	--bowtie2-debug=./bowtie2-debug \
