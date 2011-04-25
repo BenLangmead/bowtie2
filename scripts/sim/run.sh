afef4f86c4bdbc42c692c33e96724281b97b5db4
langmead <langmead@208de1ca-060f-4dda-baf0-1f374fee3c76>
2011-04-25 19:27:51 +0000

    Pass through args

--- scripts/sim/run.sh
+++ scripts/sim/run.sh
@@ -7,4 +7,5 @@ perl scripts/sim/run.pl \
 	--bowtie2=./bowtie2 \
 	--bowtie2-debug=./bowtie2-debug \
 	--bowtie2-build=./bowtie2-build \
-	--bowtie2-build-debug=./bowtie2-build-debug
+	--bowtie2-build-debug=./bowtie2-build-debug \
+	$*
