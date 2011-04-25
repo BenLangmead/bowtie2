acf55936f880f8a68aa7625362e7c7cea2bbf7c0
langmead <langmead@208de1ca-060f-4dda-baf0-1f374fee3c76>
2011-04-25 20:46:09 +0000

    Allow multiple CPUs for build step and for test

--- scripts/sim/run.sh
+++ scripts/sim/run.sh
@@ -2,10 +2,13 @@
 
 #  run.sh
 
-make bowtie2 bowtie2-debug bowtie2-build bowtie2-build-debug && \
+CPUS=$1
+shift
+make -j $CPUS bowtie2 bowtie2-debug bowtie2-build bowtie2-build-debug && \
 perl scripts/sim/run.pl \
 	--bowtie2=./bowtie2 \
 	--bowtie2-debug=./bowtie2-debug \
 	--bowtie2-build=./bowtie2-build \
 	--bowtie2-build-debug=./bowtie2-build-debug \
+	--cpus=$CPUS \
 	$*
