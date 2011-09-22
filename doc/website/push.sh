d92ada3892ef43a4492baaa81e21f3eef60db62a
langmead <langmead@208de1ca-060f-4dda-baf0-1f374fee3c76>
2011-09-22 21:39:18 +0000

    Adding website.  Small addition to NEWS.

---
+++ doc/website/push.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+##
+# push.sh
+#
+# Run this from the $CROSSBOW_HOME/doc/website subdirectory.
+#
+# Copies the files that comprise the website at
+# http://bowtie-bio.sourceforge.net/crossbow to sourceforge.  You must
+# have the right sourceforge privileges to do this.  The SF_USER
+# environment variable must be set appropriately.
+#
+
+[ -z "$SF_USER" ] && echo "Must set SF_USER" && exit 1
+
+ssh $SF_USER,bowtie-bio@web.sourceforge.net mkdir -p /home/groups/b/bo/bowtie-bio/htdocs/bowtie2
+scp -r * $SF_USER,bowtie-bio@web.sourceforge.net:/home/groups/b/bo/bowtie-bio/htdocs/bowtie2
+scp -r ../images $SF_USER,bowtie-bio@web.sourceforge.net:/home/groups/b/bo/bowtie-bio/htdocs/bowtie2/
