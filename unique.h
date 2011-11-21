8c106e4f09a316e22fa4cb1e55096b538a08ebb6
langmead <langmead@208de1ca-060f-4dda-baf0-1f374fee3c76>
2011-11-21 19:59:46 +0000

    Fixes to issues with MAPQv3 changes

--- unique.h
+++ unique.h
@@ -81,6 +81,12 @@ public:
 		char *inps) const = 0;
 };
 
+extern const TMapq unp_nosec_perf;
+extern const TMapq unp_nosec[11];
+extern const TMapq unp_sec_perf[11];
+extern const TMapq unp_sec[11][11];
+extern const TMapq pair_nosec_perf;
+
 /**
  * V3 of the MAPQ calculator
  */
@@ -108,8 +114,8 @@ public:
 		char *inps)     // put string representation of inputs here
 		const
 	{
-		if(flags.paired()) {
-			return 40;
+		if(s.paired()) {
+			return pair_nosec_perf;
 		} else {
 			bool hasSecbest = VALID_AL_SCORE(s.secbest(mate1));
 			if(!flags.canMax() && !s.exhausted(mate1) && !hasSecbest) {
@@ -121,10 +127,10 @@ public:
 			TAlScore best  = scMax - s.best(mate1).score(); // best score (lower=better)
 			size_t best_bin = (size_t)((double)best * (10.0 / (double)(scMax - scMin)) + 0.5);
 			assert_geq(best_bin, 0);
-			assert_lt(best_bin, 10);
+			assert_lt(best_bin, 11);
 			if(hasSecbest) {
-				assert_geq(best, s.secbest(mate1));
-				size_t diff = best - s.secbest(mate1);
+				assert_geq(best, s.secbest(mate1).score());
+				size_t diff = best - s.secbest(mate1).score();
 				size_t diff_bin = (size_t)((double)diff * (10.0 / (double)(scMax - scMin)) + 0.5);
 				// A valid second-best alignment was found
 				if(best == scMax) {
@@ -145,58 +151,9 @@ public:
 				}
 			}
 		}
-		return ret;
 	}
 
 protected:
-
-	//
-	// Unpaired mapping quality:
-	//
-
-	// There is no valid second-best alignment and the best alignment has a
-	// perfect score.
-	TMapq unp_nosec_perf = 44;
-	
-	// There is no valid second-best alignment.  We stratify the alignment
-	// score of the best alignment into 10 bins.
-	TMapq unp_nosec[10] = {
-		43, 42, 41, 36, 32, 27, 20, 11, 4, 1, 0
-	};
-
-	// The best alignment has a perfect score, and we stratify the distance
-	// between best and second-best alignment scores into 10 bins.
-	TMapq unp_sec_perf[10] = {
-		2, 16, 23, 30, 31, 32, 34, 36, 38, 40, 42
-	};
-
-	// The best alignment has a non-perfect score, and we stratify both by best
-	// alignment score (specifically, the maximum score minus the best "best")
-	// and by the distance between the best and second-best alignment scores
-	// ("difference").  Each is stratified into 10 bins.  Each row is a
-	// difference (smaller elts = smaller differences) and each column is a
-	// best score (smaller elts = higher best alignment scores).
-	TMapq unp_sec[10][10] = {
-		{  2,  2,  2,  1,  1, 0, 0, 0, 0, 0, 0},
-		{ 20, 14,  7,  3,  2, 1, 0, 0, 0, 0, 0},
-		{ 20, 16, 10,  6,  3, 1, 0, 0, 0, 0, 0},
-		{ 20, 17, 13,  9,  3, 1, 1, 0, 0, 0, 0},
-		{ 21, 19, 15,  9,  5, 2, 2, 0, 0, 0, 0},
-		{ 22, 21, 16, 11, 10, 5, 0, 0, 0, 0, 0},
-		{ 23, 22, 19, 16, 11, 0, 0, 0, 0, 0, 0},
-		{ 24, 25, 21, 30,  0, 0, 0, 0, 0, 0, 0},
-		{ 30, 26, 29,  0,  0, 0, 0, 0, 0, 0, 0},
-		{ 30, 27,  0,  0,  0, 0, 0, 0, 0, 0, 0},
-		{ 30,  0,  0,  0,  0, 0, 0, 0, 0, 0, 0},
-	};
-	
-	//
-	// Paired mapping quality:
-	//
-	
-	// There is no valid second-best alignment and the best alignment has a
-	// perfect score.
-	TMapq unp_nosec_perf = 44;
 
 	SimpleFunc      scoreMin_;
 	const Scoring&  sc_;
