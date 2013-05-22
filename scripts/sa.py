37d8cf66c6b607eeea962bfb7af6bedebcec5222
BenLangmead <ben.langmead@gmail.com>
2013-05-22 11:34:46 -0400

    Some documentation improvements to sa.py

--- scripts/sa.py
+++ scripts/sa.py
@@ -3,7 +3,10 @@
 """
 sa.py
 
-Parse and sanity-check a .sa file output by bowtie2-build.
+Parse and possibly sanity-check a .sa file output by bowtie2-build in --sa
+mode.  These files have a very simple format: first is a uint32_t containing
+the length of the suffix array, the rest is an array of that many uint32_ts
+containing the suffix array.
 """
 
 import sys
@@ -34,23 +37,26 @@ def loadFasta(fns):
 	return ''.join(falist)
 
 if __name__ == "__main__":
-    import argparse
-        
-    parser = argparse.ArgumentParser(\
+	import argparse
+	
+	parser = argparse.ArgumentParser(\
 		description='Parse suffix array built from bowtie2-build')
-    
-    parser.add_argument(\
-        '--sa', metavar='string', required=True, type=str,
+	
+	parser.add_argument(\
+		'--sa', metavar='string', required=True, type=str,
 		help='Suffix array file')
-    parser.add_argument(\
-        '--fa', metavar='string', type=str, nargs='+', help='FASTA file')
+	parser.add_argument(\
+		'--fa', metavar='string', type=str, nargs='+', help='FASTA file')
 
-    args = parser.parse_args()
-    
-    def go():
+	args = parser.parse_args()
+	
+	def go():
+		ref = None
 		if args.fa is not None:
 			ref = loadFasta(args.fa)
 		sas = loadBowtieSaFilename(args.sa)
+		# Suffix array is in sas; note that $ is considered greater than all
+		# other characters
 		if ref is not None:
 			for i in xrange(1, len(sas)):
 				sa1, sa2 = sas[i-1], sas[i]
@@ -67,5 +73,7 @@ if __name__ == "__main__":
 					# characters; so if these strings are tied up to the end of
 					# one or the other, the longer string is prior
 					assert sa1 < sa2, "%d, %d" % (sas[i-1], sas[i])
-		# Suffix array is in sas; note that $ is considered greater than all
-		# other characters
+			assert sas[-1] == len(ref)
+	
+	go()
+	
