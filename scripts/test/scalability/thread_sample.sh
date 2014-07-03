4fa88ab4fa06b01e7fb71e0ffdd18376cc7613bd
Valentin Antonescu <valentin@jhu.edu>
2014-07-03 10:14:17 -0400

    A few minor changes for test script

--- scripts/test/scalability/thread_sample.sh
+++ scripts/test/scalability/thread_sample.sh
@@ -7,15 +7,16 @@
 SAMPLE_POINTS=3
 MAX_THREADS=56
 CMD_SKEL="/usr/bin/time ./bowtie2 -x ../data/genomes/hg19 -U ../data/reads/ERR000589.fastq -p "
+#CMD_SKEL="/usr/bin/time -f %U,%S,%E ./bowtie2 -x ecoli_s -U example/reads/longreads.fq -p "
 DATA_DIR=$(pwd)
 DATA_DIR+="/scripts/test/scalability"
 DATA_FILE_PREFIX=$1
 
 get_sample_points() {
-    local cmd=$@
+    local cmd="$@"
     for ((i=0; i<SAMPLE_POINTS; i++))
     do
-        echo $cmd
+        echo $cmd >&2
         $($cmd)
     done
 }
@@ -37,5 +38,5 @@ collect_all_data() {
 
 collect_all_data
 
-
+# format data definitely maybe
 
