4a54af2f9f93748dea272e823b060b81c4c2e878
Valentin Antonescu <valentin@jhu.edu>
2015-05-21 13:52:38 -0400

    Ready for some test running.

--- issue20/run.sh
+++ issue20/run.sh
@@ -4,10 +4,18 @@
 # default params
 ../bowtie2 -x ~/data/hg19 -1 ERR055337_g_shuffle_1.fq.out -2 ERR055337_g_shuffle_2.fq.out --no-unal -S out_$1.sam 2>out_$1.err 
 
+#for (( i=1; i<18; i++)); do
+#  ../bowtie2 -x ~/data/hg19 -1 ERR055337_g_shuffle_1.fq.out -2 ERR055337_g_shuffle_2.fq.out --no-unal -S out_$1_p$i.sam 2>out_$1_p$i.err 
+#done
+
 # Use only system time for random generator
 #../bowtie2 -x ~/data/hg19 -1 ERR055337_g_shuffle_1.fq.out -2 ERR055337_g_shuffle_2.fq.out --no-unal --non-deterministic -S out_nd_$1.sam 2>out_nd_$1.err 
+
+#for (( i=1; i<18; i++)); do
+#  ../bowtie2 -x ~/data/hg19 -1 ERR055337_g_shuffle_1.fq.out -2 ERR055337_g_shuffle_2.fq.out --no-unal --non-deterministic -S out_nd_$1_p$i.sam 2>out_nd_$1_p$i.err 
+#done
 
 # Reporting only to check all 3 regions
 #../bowtie2 -x ~/data/hg19 -1 ERR055337_g_shuffle_1.fq.out -2 ERR055337_g_shuffle_2.fq.out -k 3 --no-unal -S out_all3_$1.sam 2>out_all_3$1.err 
  
-#
+
