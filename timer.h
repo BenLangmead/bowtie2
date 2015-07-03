6e92b8f623e94d1dc3315b708d86549c2212f14a
BenLangmead <langmea@cs.jhu.edu>
2015-07-03 15:20:28 -0400

    added an optional fine-grained timer

--- timer.h
+++ timer.h
@@ -24,9 +24,90 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#ifdef USE_FINE_TIMER
+#include <sys/time.h>
+#endif
 
 using namespace std;
 
+#ifdef USE_FINE_TIMER
+
+/* Subtract the ‘struct timeval’ values X and Y, storing the result in RESULT.
+ Return 1 if the difference is negative, otherwise 0.
+ Borrowed from: https://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
+ */
+static inline bool timeval_subtract(timeval& result, const timeval& xin, const timeval& yin) {
+	/* Perform the carry for the later subtraction by updating y. */
+	timeval x = xin;
+	timeval y = yin;
+	if (x.tv_usec < y.tv_usec) {
+		int nsec = (y.tv_usec - x.tv_usec) / 1000000 + 1;
+		y.tv_usec -= 1000000 * nsec;
+		y.tv_sec += nsec;
+	}
+	if (x.tv_usec - y.tv_usec > 1000000) {
+		int nsec = (x.tv_usec - y.tv_usec) / 1000000;
+		y.tv_usec += 1000000 * nsec;
+		y.tv_sec -= nsec;
+	}
+
+	/* Compute the time remaining to wait. tv_usec is certainly positive. */
+	result.tv_sec = x.tv_sec - y.tv_sec;
+	result.tv_usec = x.tv_usec - y.tv_usec;
+
+	/* Return 1 if result is negative. */
+	return x.tv_sec < y.tv_sec;
+}
+
+/**
+ * Use gettimeofday() call to keep track of elapsed time between creation and
+ * destruction.  If verbose is true, Timer will print a message showing
+ * elapsed time to the given output stream upon destruction.
+ */
+class Timer {
+public:
+	Timer(ostream& out = cout, const char *msg = "", bool verbose = true) :
+		_t(), _out(out), _msg(msg), _verbose(verbose)
+	{
+		gettimeofday(&_t, NULL);
+	}
+
+	/// Optionally print message
+	~Timer() {
+		if(_verbose) write(_out);
+	}
+
+	/// Return elapsed time since Timer object was created
+	time_t elapsed() const {
+		timeval f;
+		gettimeofday(&f, NULL);
+		return f.tv_sec - _t.tv_sec;
+	}
+
+	void write(ostream& out) {
+		timeval f;
+		gettimeofday(&f, NULL);
+		timeval diff;
+		timeval_subtract(diff, f, _t);
+		time_t hours   = (diff.tv_sec / 60) / 60;
+		time_t minutes = (diff.tv_sec / 60) % 60;
+		time_t seconds = (diff.tv_sec % 60);
+		time_t milliseconds = (diff.tv_usec / 1000);
+		std::ostringstream oss;
+		oss << _msg << setfill ('0') << setw (2) << hours << ":"
+			<< setfill ('0') << setw (2) << minutes << ":"
+			<< setfill ('0') << setw (2) << seconds << "."
+			<< setfill ('0') << setw (3) << milliseconds << endl;
+		out << oss.str().c_str();
+	}
+
+private:
+	timeval     _t;
+	ostream&    _out;
+	const char *_msg;
+	bool        _verbose;
+};
+#else
 /**
  * Use time() call to keep track of elapsed time between creation and
  * destruction.  If verbose is true, Timer will print a message showing
@@ -50,14 +131,14 @@ public:
 	void write(ostream& out) {
 		time_t passed = elapsed();
 		// Print the message supplied at construction time followed
-		// by time elapsed formatted HH:MM:SS 
+		// by time elapsed formatted HH:MM:SS
 		time_t hours   = (passed / 60) / 60;
 		time_t minutes = (passed / 60) % 60;
 		time_t seconds = (passed % 60);
 		std::ostringstream oss;
 		oss << _msg << setfill ('0') << setw (2) << hours << ":"
-		           << setfill ('0') << setw (2) << minutes << ":"
-		           << setfill ('0') << setw (2) << seconds << endl;
+			<< setfill ('0') << setw (2) << minutes << ":"
+			<< setfill ('0') << setw (2) << seconds << endl;
 		out << oss.str().c_str();
 	}
 	
@@ -67,6 +148,7 @@ private:
 	const char *_msg;
 	bool        _verbose;
 };
+#endif // USE_FINE_TIMER
 
 static inline void logTime(std::ostream& os, bool nl = true) {
 	struct tm *current;
