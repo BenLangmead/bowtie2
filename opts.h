870a12da3e03065f7149cc77b353b22fe21506b2
Rone Charles <rone_charles@fastmail.com>
2017-03-03 23:14:55 -0500

    Fix indenting

--- opts.h
+++ opts.h
@@ -152,7 +152,7 @@ enum {
 	ARG_DESC_FMOPS,             // --desc-fmops
 	ARG_LOG_DP,                 // --log-dp
 	ARG_LOG_DP_OPP,             // --log-dp-opp
-	ARG_INTERLEAVED_FASTQ 		// --interleaved
+	ARG_INTERLEAVED_FASTQ       // --interleaved
 };
 
 #endif
