2ee98775fd1595324cf8beda623d52ad54f882b8
BenLangmead <langmea@cs.jhu.edu>
2017-07-25 16:32:15 -0400

    nudge more toward master-branch functionality w/r/t compressed reads

--- pat.h
+++ pat.h
@@ -342,6 +342,7 @@ public:
 		infiles_(infiles),
 		filecur_(0),
 		fp_(NULL),
+		zfp_(NULL),
 		is_open_(false),
 		first_(true),
 		compressed_(false)
@@ -358,8 +359,14 @@ public:
 	 */
 	virtual ~CFilePatternSource() {
 		if(is_open_) {
+			if (compressed_) {
+				assert(zfp_ != NULL);
+				gzclose(zfp_);
+			}
+			else {
 				assert(fp_ != NULL);
 				fclose(fp_);
+			}
 		}
 	}
 
@@ -408,13 +415,11 @@ protected:
 	void open();
 
 	int getc_wrapper() {
-		//return compressed_ ? gzgetc(zfp_) : getc_unlocked(fp_);
-		return getc_unlocked(fp_);
+		return compressed_ ? gzgetc(zfp_) : getc_unlocked(fp_);
 	}
 
 	int ungetc_wrapper(int c) {
-		//return compressed_ ? gzungetc(c, zfp_) : ungetc(c, fp_);
-		return ungetc(c, fp_);
+		return compressed_ ? gzungetc(c, zfp_) : ungetc(c, fp_);
 	}
 
 	bool is_gzipped_file(const std::string& filename) {
@@ -438,6 +443,7 @@ protected:
 	EList<bool> errs_;		 // whether we've already printed an error for each file
 	size_t filecur_;		 // index into infiles_ of next file to read
 	FILE *fp_;				 // read file currently being read from
+	gzFile zfp_;			 // compressed version of fp_
 	bool is_open_;			 // whether fp_ is currently open
 	bool first_;			 // parsing first record in first file?
 	char buf_[64*1024];		 // file buffer
