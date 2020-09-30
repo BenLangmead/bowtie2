b9a9151cd85f15625bb37b0fe612b208d5f06cfd
Ben Langmead <langmea@cs.jhu.edu>
2020-09-30 09:09:53 -0400

    take 300K reads

--- seed-expts/reads/truncate.bash
+++ seed-expts/reads/truncate.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for fn in *_local.fastq *_semiglobal.fastq ; do
-	newfn=$(echo ${fn} | sed 's/\.fastq$/_100k.fastq/')
-	head -n 400000 ${fn} > ${newfn}
+	newfn=$(echo ${fn} | sed 's/\.fastq$/_300k.fastq/')
+	head -n 1200000 ${fn} > ${newfn}
 done
