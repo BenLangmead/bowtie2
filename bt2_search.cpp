8f47f65f85a9a6f0ec08154cb5759e14329e67e5
Igor Sfiligoi <isfiligoi@sdsc.edu>
2025-12-30 20:03:58 -0800

    Add deterministicSeeds option

--- bt2_search.cpp
+++ bt2_search.cpp
@@ -85,6 +85,7 @@ static string metricsFile;// output file to put alignment metrics in
 static bool metricsStderr;// output file to put alignment metrics in
 static bool metricsPerRead; // report a metrics tuple for every read
 static bool allHits;      // for multihits, report just one
+static bool deterministicSeeds;      // for low quality seeds, enable subsampling
 static bool showVersion;  // just print version and quit?
 static int ipause;        // pause before maching?
 static uint64_t qUpto;    // max # of queries to read
@@ -299,6 +300,7 @@ static void resetOptions() {
 	metricsStderr       = false;	// print metrics to stderr (in addition to --metrics-file if it's specified
 	metricsPerRead      = false;	// report a metrics tuple for every read?
 	allHits		    = false;	// for multihits, report just one
+	deterministicSeeds  = false;    // for low quality seeds, enable subsampling
 	showVersion	    = false;	// just print version and quit?
 	ipause		    = 0;	// pause before maching?
 	qUpto               = 0xffffffffffffffff; // max # of queries to read
@@ -480,7 +482,7 @@ static void resetOptions() {
 #endif
 }
 
-static const char *short_options = "bfF:qbzhcu:rv:s:aP:t3:5:w:p:k:l:M:1:2:I:X:CQ:N:i:L:U:x:S:g:O:D:R:";
+static const char *short_options = "bfF:qbzhcu:rv:s:adP:t3:5:w:p:k:l:M:1:2:I:X:CQ:N:i:L:U:x:S:g:O:D:R:";
 
 static struct option long_options[] = {
 	{(char*)"verbose",                     no_argument,        0,                   ARG_VERBOSE},
@@ -631,6 +633,8 @@ static struct option long_options[] = {
 	{(char*)"no-exact-upfront",            no_argument,        0,                   ARG_EXACT_UPFRONT_NO},
 	{(char*)"no-1mm-upfront",              no_argument,        0,                   ARG_1MM_UPFRONT_NO},
 	{(char*)"1mm-minlen",                  required_argument,  0,                   ARG_1MM_MINLEN},
+	{(char*)"deterministic-seeds",         no_argument,        0,                   'd'},
+	{(char*)"no-deterministic-seeds",      no_argument,        0,                   ARG_DET_SEEDS_NO},
 	{(char*)"seed-off",                    required_argument,  0,                   'O'},
 	{(char*)"seed-boost",                  required_argument,  0,                   ARG_SEED_BOOST_THRESH},
 	{(char*)"read-times",                  no_argument,        0,                   ARG_READ_TIMES},
@@ -1323,6 +1327,8 @@ static void parseOption(int next_option, const char *arg) {
 		}
 		break;
 	}
+	case 'd': deterministicSeeds = true; break;
+	case ARG_DET_SEEDS_NO: deterministicSeeds = false; break;
 	case ARG_VERBOSE: gVerbose = 1; break;
 	case ARG_STARTVERBOSE: startVerbose = true; break;
 	case ARG_QUIET: gQuiet = true; break;
